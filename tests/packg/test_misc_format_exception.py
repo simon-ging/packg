255db890d8ef286baab28d7b18be1ba30d3507e1
gings <s@ging.ai>
2023-12-04 00:24:22 +0100

    autoupdate

--- tests/packg/test_misc_format_exception.py
+++ tests/packg/test_misc_format_exception.py
@@ -9,11 +9,12 @@ def test_format_exception():
         e_out = format_exception(e, with_traceback=True)
         print()
         print(e_out)
-        assert e_out.startswith("  File \"")
-        assert e_out.endswith("in test_format_exception\n    raise ValueError(f\"{var}\")"
-                              "\nValueError: test")
+        assert e_out.startswith('  File "'), e_out
+        assert e_out.endswith(
+            'in test_format_exception\n    raise ValueError(f"{var}")\nValueError: test'
+        ), e_out
 
         e_out = format_exception(e, with_traceback=False)
         print()
         print(e_out)
-        assert e_out == "ValueError: test"
+        assert e_out == "ValueError: test", e_out
