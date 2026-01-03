3a7794a510a3e974e5cfb4e6230f38d48cd7bd92
gings <s@ging.ai>
2026-01-03 12:58:38 +0100

    autoupdate

--- src/packg/__init__.py
+++ src/packg/__init__.py
@@ -2,4 +2,4 @@ from .constclass import Const
 from .misc import format_exception, format_exception_with_chain
 
 __all__ = ["Const", "format_exception", "format_exception_with_chain"]
-__version__ = "0.33.12"
+__version__ = "0.33.13"
